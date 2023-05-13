a2fcbdf0f1255485dee28d90d69a2430594cb99f
Ondřej Veverka <ondrick.v@gmail.com>
2023-05-13 20:01:26 +0200

    Ondra-13/05/2023

---
+++ src/features/stagesSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+import data from "../data.json";
+
+// Extract the stages list from the imported data file
+const stagesList = data.stages
+
+// Set the initial state of the stages slice to the stagesList
+const initialStateValue = stagesList
+
+// A Redux slice for managing the state of the stages
+export const stagesSlice = createSlice({
+    name: "stages",
+    initialState: initialStateValue,
+    reducers: {
+        // A reducer that adds a new stage to the stages state array
+        addStage: (state, action) => {
+            const newStage = action.payload
+    
+            state.push(newStage)
+            return state
+        },
+    },
+})
+
+// Export the addStage action creator from the stagesSlice
+export const { addStage } = stagesSlice.actions
+
+// Export the stagesSlice reducer
+export default stagesSlice.reducer
