ae6acb7120a17ab8b45445a5208dea7c1a93da43
Ondřej Veverka <ondrick.v@gmail.com>
2023-06-07 15:04:43 +0200

    DeleteStageButtonILIKETOFLIRT

--- src/components/DeleteStageButton.js
+++ src/components/DeleteStageButton.js
@@ -4,13 +4,27 @@ Button pro smazání etapy projektu
 Button pro vytvoření vazby (prev, next) mezi etapami
  */
 import { Trash } from "react-bootstrap-icons"
+import { useState } from "react"
 // import { useDispatch } from "react-redux";
 
 
 export const DeleteStageButton = ({ stageId }) => {
     // const dispatch = useDispatch()
+    [deleteVisibility, setDeleteVisibility] = useState(false)
 
-    return(
-        <button className="btn btn-danger btn-sm" onClick={() => { }}><Trash/></button>
-    )
+    if(deleteVisibility === false)
+    {
+        return(
+            <button className="btn btn-danger btn-sm" onClick={() => {setDeleteVisibility(!deleteVisibility)}}><Trash/></button>
+        )
+    }
+    else
+    {
+        return(
+            <div>
+                <button className="btn btn-outline-danger btn-sm" onClick={() => {setDeleteVisibility(!deleteVisibility)}}>Cancel</button>
+                <button className="btn btn-outline-danger btn-sm" onClick={() => {/*Funkce co smaže stage*/}}><Trash/></button>
+            </div>   
+        )
+    }
 }
